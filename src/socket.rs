1057ef2853112b7a290f238144885df35664db48
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2024-01-13 08:25:26 +1100

    Now exposing another method

--- src/socket.rs
+++ src/socket.rs
@@ -629,7 +629,7 @@ impl Socket {
     /// but suppresses the `WSAEMSGSIZE` error on Windows.
     ///
     /// [`peek_from`]: Socket::peek_from
-    #[cfg(not(target_os = "wasi"))]
+    #[cfg(not(all(target_os = "wasi", target_vendor = "wasmer")))]
     pub fn peek_sender(&self) -> io::Result<SockAddr> {
         sys::peek_sender(self.as_raw())
     }
